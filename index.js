81dbcac7af84ad5d58a59542f2dec26010719f74
Tilux <tilux001@gmail.com>
2025-06-25 09:24:59 +0100

    Fisrt node class

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express()
+
+const port = 5500
+students = "/student"
+
+const allStudent = [
+    {id:214723, name:'Adekola Israel', Department: "Software Engineer"},
+    {id:214323, name:"Bamidele mercy", Department: "Cyber Security"},
+    {id:214723, name:'Adekola Israel', Department: "Software Engineer"},
+    {id:214323, name:"Bamidele mercy", Department: "Cyber Security"},
+    {id:214723, name:'Adekola Israel', Department: "Software Engineer"},
+    {id:214323, name:"Bamidele mercy", Department: "Cyber Security"},
+    
+]
+
+app.get(students,(req,res)=>{
+    res.send(allStudent)
+})
+
+app.get("/",()=>{
+    res.send("Hello world")
+})
+
+
+app.listen(port, ()=>{
+    console.log("Hello world");
+})
